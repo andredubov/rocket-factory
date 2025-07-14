87f8875fa4821a14ebba5662f814af7043beda65
Andrei Dubov <av.dubov@yandex.ru>
2025-07-14 23:42:19 +0400

    refactor: update inventory service

--- inventory/internal/repository/converter/part.go
+++ inventory/internal/repository/converter/part.go
@@ -7,27 +7,6 @@ import (
 
 // PartToRepoModel converts a domain model (repo.Part) to a repository model (repoModel.Part).
 func PartToRepoModel(part model.Part) repoModel.Part {
-	// Convert metadata from model to repoModel
-	metadata := make(map[string]repoModel.Value)
-	for k, v := range part.Metadata {
-		var repoValue repoModel.Value
-
-		if v.StringValue != nil {
-			repoValue.StringValue = v.StringValue
-		}
-		if v.Int64Value != nil {
-			repoValue.Int64Value = v.Int64Value
-		}
-		if v.DoubleValue != nil {
-			repoValue.DoubleValue = v.DoubleValue
-		}
-		if v.BoolValue != nil {
-			repoValue.BoolValue = v.BoolValue
-		}
-
-		metadata[k] = repoValue
-	}
-
 	return repoModel.Part{
 		Uuid:          part.Uuid,
 		Name:          part.Name,
@@ -47,7 +26,7 @@ func PartToRepoModel(part model.Part) repoModel.Part {
 			Website: part.Manufacturer.Website,
 		},
 		Tags:      part.Tags,
-		Metadata:  metadata, // Используем сконвертированные метаданные
+		Metadata:  convertMetadataToRepo(part.Metadata),
 		CreatedAt: part.CreatedAt,
 		UpdatedAt: part.UpdatedAt,
 	}
@@ -55,27 +34,6 @@ func PartToRepoModel(part model.Part) repoModel.Part {
 
 // PartToModel converts a repository model (repoModel.Part) to a domain model (model.Part).
 func PartToModel(part repoModel.Part) model.Part {
-	// Convert metadata from repoModel to model
-	metadata := make(map[string]model.Value)
-	for k, v := range part.Metadata {
-		var modelValue model.Value
-
-		if v.StringValue != nil {
-			modelValue.StringValue = v.StringValue
-		}
-		if v.Int64Value != nil {
-			modelValue.Int64Value = v.Int64Value
-		}
-		if v.DoubleValue != nil {
-			modelValue.DoubleValue = v.DoubleValue
-		}
-		if v.BoolValue != nil {
-			modelValue.BoolValue = v.BoolValue
-		}
-
-		metadata[k] = modelValue
-	}
-
 	return model.Part{
 		Uuid:          part.Uuid,
 		Name:          part.Name,
@@ -95,8 +53,56 @@ func PartToModel(part repoModel.Part) model.Part {
 			Website: part.Manufacturer.Website,
 		},
 		Tags:      part.Tags,
-		Metadata:  metadata, // Используем сконвертированные метаданные
+		Metadata:  convertMetadataToModel(part.Metadata),
 		CreatedAt: part.CreatedAt,
 		UpdatedAt: part.UpdatedAt,
 	}
+}
+
+// convertMetadataToRepo converts metadata values from domain model format to repository model format.
+func convertMetadataToRepo(metadata map[string]model.Value) map[string]repoModel.Value {
+	result := make(map[string]repoModel.Value)
+	for k, v := range metadata {
+		var repoValue repoModel.Value
+
+		if v.StringValue != nil {
+			repoValue.StringValue = v.StringValue
+		}
+		if v.Int64Value != nil {
+			repoValue.Int64Value = v.Int64Value
+		}
+		if v.DoubleValue != nil {
+			repoValue.DoubleValue = v.DoubleValue
+		}
+		if v.BoolValue != nil {
+			repoValue.BoolValue = v.BoolValue
+		}
+
+		result[k] = repoValue
+	}
+	return result
+}
+
+// convertMetadataToModel converts metadata values from repository model format to domain model format.
+func convertMetadataToModel(metadata map[string]repoModel.Value) map[string]model.Value {
+	result := make(map[string]model.Value)
+	for k, v := range metadata {
+		var modelValue model.Value
+
+		if v.StringValue != nil {
+			modelValue.StringValue = v.StringValue
+		}
+		if v.Int64Value != nil {
+			modelValue.Int64Value = v.Int64Value
+		}
+		if v.DoubleValue != nil {
+			modelValue.DoubleValue = v.DoubleValue
+		}
+		if v.BoolValue != nil {
+			modelValue.BoolValue = v.BoolValue
+		}
+
+		result[k] = modelValue
+	}
+	return result
 }
