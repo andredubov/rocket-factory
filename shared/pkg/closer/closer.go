347696d385c27dd690e5f90c337e2d575fcdcda4
Andrei Dubov <av.dubov@yandex.ru>
2025-08-07 17:14:46 +0400

    docs(closer): add comprehensive package documentation

--- shared/pkg/closer/closer.go
+++ shared/pkg/closer/closer.go
@@ -7,27 +7,40 @@ import (
 	"sync"
 )
 
+// globalCloser is the default instance used by package-level functions.
 var globalCloser = New()
 
+// Add registers one or more cleanup functions with the global closer.
+// These functions will be executed when CloseAll is called, in the order they were added.
+// Each function should return an error if the cleanup fails.
 func Add(f ...func() error) {
 	globalCloser.Add(f...)
 }
 
+// Wait blocks until all cleanup functions registered with the global closer have completed.
+// Typically called after CloseAll to wait for cleanup to finish.
 func Wait() {
 	globalCloser.Wait()
 }
 
+// CloseAll executes all registered cleanup functions with the global closer.
+// Functions are executed concurrently, and CloseAll is idempotent (will only run once).
 func CloseAll() {
 	globalCloser.CloseAll()
 }
 
+// Closer manages a collection of cleanup functions and provides
+// thread-safe methods for graceful shutdown management.
 type Closer struct {
-	mu    sync.Mutex
-	once  sync.Once
-	done  chan struct{}
-	funcs []func() error
+	mu    sync.Mutex     // protects funcs slice
+	once  sync.Once      // ensures CloseAll only runs once
+	done  chan struct{}  // signals when closing is complete
+	funcs []func() error // registered cleanup functions
 }
 
+// New creates a new Closer instance. If signals are provided,
+// it starts a goroutine that will trigger CloseAll when any of
+// those signals are received.
 func New(sig ...os.Signal) *Closer {
 	c := &Closer{done: make(chan struct{})}
 	if len(sig) > 0 {
@@ -42,16 +55,23 @@ func New(sig ...os.Signal) *Closer {
 	return c
 }
 
+// Add registers one or more cleanup functions with this Closer instance.
+// Functions are appended to the existing list of cleanup functions.
 func (c *Closer) Add(f ...func() error) {
 	c.mu.Lock()
 	c.funcs = append(c.funcs, f...)
 	c.mu.Unlock()
 }
 
+// Wait blocks until CloseAll has completed execution of all registered functions.
+// Returns immediately if CloseAll hasn't been called yet.
 func (c *Closer) Wait() {
 	<-c.done
 }
 
+// CloseAll executes all registered cleanup functions concurrently.
+// This method is idempotent - subsequent calls will have no effect.
+// Errors from cleanup functions are logged but otherwise ignored.
 func (c *Closer) CloseAll() {
 	c.once.Do(func() {
 		defer close(c.done)
